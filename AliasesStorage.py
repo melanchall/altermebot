95c2ce4e6163aad79f7035a861a9726aea4eb4e5
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-02-02 22:44:06 +0300

    Manage users by id

--- AliasesStorage.py
+++ AliasesStorage.py
@@ -7,7 +7,7 @@ class AliasesStorage(object):
     """description of class"""
 
     def __init__(self):
-        self._connection = sqlite3.connect("aliases.db", check_same_thread=False)
+        self._connection = sqlite3.connect("aliases2.db", check_same_thread=False)
         self._connection.set_trace_callback(logging.info)
         self._connection.create_function('regexp', 2, self.__regexp)
         self._connection.create_function('eqnocase', 2, self.__eqnocase)
@@ -16,15 +16,14 @@ class AliasesStorage(object):
 
         self.__create_tables()
 
-    def get_aliases_count(self, username, chat_id):
+    def get_aliases_count(self, user_id, chat_id):
         (count,) = self._cursor.execute('''SELECT COUNT(*)
-                                           FROM users, aliases
-                                           WHERE EQNOCASE(username, ?) AND
-                                                 user_id = users.id AND
-                                                 chat_id = ?''', (username, chat_id)).fetchone()
+                                           FROM aliases
+                                           WHERE user_id = ? AND
+                                                 chat_id = ?''', (user_id, chat_id)).fetchone()
         return count
 
-    def check_alias(self, username, chat_id, alias):
+    def check_alias_is_not_in_use(self, user_id, chat_id, alias):
         user_id_row = self._cursor.execute('''SELECT user_id
                                               FROM aliases
                                               WHERE chat_id = ? AND
@@ -32,58 +31,40 @@ class AliasesStorage(object):
         if user_id_row is None:
             return True
 
-        username_row = self._cursor.execute('''SELECT username
-                                               FROM users
-                                               WHERE id = ?''', (user_id_row[0],)).fetchone()
-        return username_row[0].lower() == username.lower()
+        return user_id_row[0] == user_id
 
-    def add_alias(self, username, chat_id, alias):
-        self._cursor.execute("INSERT OR IGNORE INTO users(username) VALUES (?)", (username,))
-        self._connection.commit()
-
-        user_id = self._cursor.execute("SELECT id FROM users WHERE EQNOCASE(username, ?)", (username,)).fetchone()[0]
+    def add_alias(self, user_id, chat_id, alias):
         self._cursor.execute('''INSERT OR IGNORE INTO aliases(user_id, chat_id, alias)
                                 VALUES (?, ?, ?)''', (user_id, chat_id, alias))
         self._connection.commit()
 
-    def remove_alias(self, username, chat_id, alias):
+    def remove_alias(self, user_id, chat_id, alias):
         self._cursor.execute('''DELETE
                                 FROM aliases
-                                WHERE user_id IN (SELECT id
-                                                  FROM users
-                                                  WHERE EQNOCASE(username, ?)) AND
+                                WHERE user_id = ? AND
                                       EQNOCASE(alias, ?) AND
-                                      chat_id = ?''', (username, alias, chat_id))
+                                      chat_id = ?''', (user_id, alias, chat_id))
         self._connection.commit()
 
-    def remove_all_aliases(self, username, chat_id):
+    def remove_all_aliases(self, user_id, chat_id):
         self._cursor.execute('''DELETE
                                 FROM aliases
-                                WHERE user_id IN (SELECT id
-                                                  FROM users
-                                                  WHERE EQNOCASE(username, ?)) AND
-                                      chat_id = ?''', (username, chat_id))
+                                WHERE user_id = ? AND
+                                      chat_id = ?''', (user_id, chat_id))
         self._connection.commit()
 
-    def get_aliases(self, username, chat_id):
-        rows = self._cursor.execute('''SELECT aliases.alias
-                                       FROM users, aliases
-                                       WHERE EQNOCASE(username, ?) AND
-                                             user_id = users.id AND
-                                             chat_id = ?''', (username, chat_id)).fetchall()
+    def get_aliases(self, user_id, chat_id):
+        rows = self._cursor.execute('''SELECT alias
+                                       FROM aliases
+                                       WHERE user_id = ? AND
+                                             chat_id = ?''', (user_id, chat_id)).fetchall()
         return list(map(lambda row: row[0], rows))
 
     def contains_alias(self, text, chat_id):
-        rows = self._cursor.execute('''SELECT username
-                                       FROM users, aliases
-                                       WHERE user_id = users.id AND
-                                             chat_id = ? AND
+        rows = self._cursor.execute('''SELECT user_id
+                                       FROM aliases
+                                       WHERE chat_id = ? AND
                                              alias REGEXP ? ''', (chat_id, text)).fetchall()
-        return list(map(lambda row: row[0], rows))
-
-    def get_chats(self):
-        rows = self._cursor.execute('''SELECT DISTINCT chat_id
-                                       FROM aliases''').fetchall()
         return list(map(lambda row: row[0], rows))
 
     @staticmethod
@@ -95,13 +76,7 @@ class AliasesStorage(object):
         return 1 if x.lower() == y.lower() else 0
 
     def __create_tables(self):
-        self.__create_users_table()
         self.__create_aliases_table()
-
-    def __create_users_table(self):
-        self._cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                                id       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                                username TEXT NOT NULL UNIQUE)''')
 
     def __create_aliases_table(self):
         self._cursor.execute('''CREATE TABLE IF NOT EXISTS aliases (
@@ -109,5 +84,4 @@ class AliasesStorage(object):
                                 user_id INTEGER NOT NULL,
                                 chat_id INTEGER NOT NULL,
                                 alias   TEXT NOT NULL,
-                                FOREIGN KEY(user_id) REFERENCES users(id),
                                 UNIQUE (user_id, chat_id, alias))''')
--- CommandHandlers/AliasCommandHandler.py
+++ CommandHandlers/AliasCommandHandler.py
@@ -14,25 +14,28 @@ class AliasCommandHandler(CommandHandler):
         self._aliases_storage = aliases_storage
 
     def __handle(self, bot, update, args):
-        logging.info('/alias: entered by user: %d' % update.message.from_user.id)
+        logging.info('/alias: entered')
 
         message = update.message
         chat_id = message.chat_id
-        from_username = message.from_user.username
+        user_id = message.from_user.id
 
-        if not from_username:
-            bot.send_message(chat_id=chat_id,
-                             text="[Mr. Unknown](tg://user?id=%d), are you tagged?" % update.message.from_user.id,
-                             parse_mode=ParseMode.MARKDOWN)
-            logging.info('/alias: exited due to no username')
-            return
+        user = message.from_user
+        mention = user.username
+        parse_mode = None
+        if not mention:
+            mention = '[Unknown human](tg://user?id=%d)' % user_id
+            parse_mode = ParseMode.MARKDOWN
+        else:
+            mention = "@%s" % mention
 
         # Check if max aliases count reached
 
-        if self._aliases_storage.get_aliases_count(from_username, chat_id) >= ALIASES_MAX_COUNT:
+        if self._aliases_storage.get_aliases_count(user_id, chat_id) >= ALIASES_MAX_COUNT:
             bot.send_message(chat_id=chat_id,
-                             text="@%s, you've reached max aliases count for this chat (%d). Please remove some "
-                                  "aliases and try again" % (from_username, ALIASES_MAX_COUNT))
+                             text="%s, you've reached max aliases count for this chat (%d). Please remove some "
+                                  "aliases and try again" % (mention, ALIASES_MAX_COUNT),
+                             parse_mode=parse_mode)
             logging.info('/alias: exited due to max aliases count reached')
             return
 
@@ -40,8 +43,9 @@ class AliasCommandHandler(CommandHandler):
 
         if not any(args):
             bot.send_message(chat_id=chat_id,
-                             text="@%s, you should specify alias passing it as a parameter to the /alias command"
-                                  % from_username)
+                             text="%s, you should specify alias passing it as a parameter to the /alias command"
+                                  % mention,
+                             parse_mode=parse_mode)
             logging.info('/alias: exited due to no arguments provided')
             return
 
@@ -51,13 +55,15 @@ class AliasCommandHandler(CommandHandler):
 
         if any(e.type == MessageEntity.MENTION for e in entities):
             bot.send_message(chat_id=chat_id,
-                             text="@%s, alias cannot contain mentions" % from_username)
+                             text="%s, alias cannot contain mentions" % mention,
+                             parse_mode=parse_mode)
             logging.info('/alias: exited due to message contains mentions')
             return
 
         if any(e.type == MessageEntity.BOT_COMMAND and e.offset > 0 for e in entities):
             bot.send_message(chat_id=chat_id,
-                             text="@%s, alias cannot contain bot commands" % from_username)
+                             text="%s, alias cannot contain bot commands" % mention,
+                             parse_mode=parse_mode)
             logging.info('/alias: exited due to message contains bot commands')
             return
 
@@ -67,35 +73,38 @@ class AliasCommandHandler(CommandHandler):
 
         if len(alias) < ALIAS_MIN_LENGTH:
             bot.send_message(chat_id=chat_id,
-                             text="@%s, alias is too short. Min length is %d"
-                                  % (from_username, ALIAS_MIN_LENGTH))
+                             text="%s, alias is too short. Min length is %d"
+                                  % (mention, ALIAS_MIN_LENGTH),
+                             parse_mode=parse_mode)
             logging.info('/alias: exited due to alias is too short')
             return
 
         if len(alias) > ALIAS_MAX_LENGTH:
             bot.send_message(chat_id=chat_id,
-                             text="@%s, alias is too long. Max length is %d"
-                                  % (from_username, ALIAS_MAX_LENGTH))
+                             text="%s, alias is too long. Max length is %d"
+                                  % (mention, ALIAS_MAX_LENGTH),
+                             parse_mode=parse_mode)
             logging.info('/alias: exited due to alias is too long')
             return
 
         # Check that alias is not in use
 
-        if not self._aliases_storage.check_alias(from_username, chat_id, alias):
+        if not self._aliases_storage.check_alias_is_not_in_use(user_id, chat_id, alias):
             bot.send_message(chat_id=chat_id,
-                             text="@%s, alias *%s* is already in use by another user"
-                                  % (escape_markdown(from_username), escape_markdown(alias)),
+                             text="%s, alias *%s* is already in use by another user"
+                                  % (escape_markdown(mention) if parse_mode is None else mention,
+                                     escape_markdown(alias)),
                              parse_mode=ParseMode.MARKDOWN)
             logging.info('/alias: exited due to alias is already in use by another user')
             return
 
         # Add alias
 
-        self._aliases_storage.add_alias(from_username, chat_id, alias)
+        self._aliases_storage.add_alias(user_id, chat_id, alias)
 
         bot.send_message(chat_id=chat_id,
-                         text="@%s, alias *%s* was successfully added"
-                              % (escape_markdown(from_username), escape_markdown(alias)),
+                         text="%s, alias *%s* was successfully added"
+                              % (escape_markdown(mention) if parse_mode is None else mention, escape_markdown(alias)),
                          parse_mode=ParseMode.MARKDOWN)
 
         logging.info('/alias: exited')
