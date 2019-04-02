41c6fe5cad287ae6e1f0cd0ffc4ffa6518db718d
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-04-02 14:20:43 +0300

    Fix language switching

--- Localization/Localizer.py
+++ Localization/Localizer.py
@@ -9,8 +9,8 @@ class Localizer:
         message = update.message
         chat_id = message.chat_id
         user_id = message.from_user.id
-        language = self._db_manager.get_language(user_id, chat_id)
 
         def localize(string_id):
+            language = self._db_manager.get_language(user_id, chat_id)
             return Strings.CONTENT[language][string_id]
         return localize
